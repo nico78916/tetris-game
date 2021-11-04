3b395515763abdbb249459aa587e5ca5a241a3cb
kevin42000 <93095916+kevin42000@users.noreply.github.com>
2021-11-04 17:05:02 +0100

    Update game.c

--- module/game.c
+++ module/game.c
@@ -188,7 +188,7 @@ game init_game(game g){
 int est_fini(game g){
     int i;
     figure grid;
-    grid = game->grid
+    grid = g->grid
     for(i=0;i<NB_COLS;i++){
         if(grid[0][i]!=0){
             return 1;       /*Si return 1 alors il y a un bloque sur la première ligne, le jeu est fini*/
@@ -200,7 +200,7 @@ int est_fini(game g){
 int verif_sienbas(game g){
     int i, j;
     figure grid;
-    grid = game->grid;
+    grid = g->grid;
     for(i=1;i<NB_LIGNES;i++){
         for(j=1;j<NN_COLS;j++){
             if(grid[i][j] == 1 && grid[i-1][j] != 0 && grid[i-1][j] != 1){
@@ -214,7 +214,7 @@ int verif_sienbas(game g){
 int verif_lignecomplete(game g){				/*pour chaque ligne, vérifie si la première valeur de la ligne est 1*/
     int i, j = 0, k, verif[NB_LIGNES]={0};		/*si elle vaut 1 alors ça check toute les valeurs de la ligne*/
     figure grid;								/*j fait le count et compte le nombre d'itération de 1*/
-    grid = game->grid;							/*si j est égal au NB_COLS, alors toute la ligne vaut 1*/
+    grid = g->grid;							/*si j est égal au NB_COLS, alors toute la ligne vaut 1*/
     for(i=0;i<NB_LIGNES;i++){
         if(grid[i][j]==1){
             for(k=0;k<NB_COLS;k++){
@@ -233,7 +233,7 @@ int verif_lignecomplete(game g){				/*pour chaque ligne, vérifie si la premièr
 void ligne_complete(game g, int verif[NB_LIGNES]){		/*efface une ligne complète*/
 	int i, j;
 	figure grid;
-	grid = game->grid;
+	grid = g->grid;
 	for(i=0;i<NB_LIGNES;i++){
 		if(verif[i]==1){
 			for(j=0;j<NB_COLS;j++){
@@ -246,7 +246,7 @@ void ligne_complete(game g, int verif[NB_LIGNES]){		/*efface une ligne complète
 int verif_jeufini(game g){
 	int i;
 	figure grid;
-	grid = game->grid;
+	grid = g->grid;
 	for(i=0;i<NB_COLS;i++){
 		if(grid[0][i]==1){
 			return 1;
@@ -258,8 +258,8 @@ int verif_jeufini(game g){
 void descente(game g){
 	int i, j;
 	figure grid;
-	grid = game->grid;
-	for(i=NB_LIGNES-1;i=0;i--){
+	grid = g->grid;
+	for(i=NB_LIGNES-1;i>=0;i--){
 		for(j=0;j<NB_COLS;j++){
 			if(grid[i][j]!=0 && grid[i][j]!=1){
 				grid[i+1][j] = grid[i][j];
@@ -268,3 +268,42 @@ void descente(game g){
 		}
 	}
 }
+
+void mouv_gauche(game g){
+	int i, j;
+	figure grid;
+	grid = g->grid;
+	for(i=0;i<NB_LIGNES;i++){
+		if(grid[i][0]!=0 && grid[i][0]!=1){
+			return 0;
+		}
+	}
+	for(j=0;j<NB_COLS;j++){
+		for(i=0;j<NB_LIGNES;i++){
+			if(grid[i][j]!=0 && grid[i][j]!=1){
+				grid[i][j-1] = grid[i][j];
+				grid[i][j] = 0;
+			}
+		}
+	}
+}
+
+void mouv_droite(game g){
+	int i, j;
+	figure grid;
+	grid = g->grid;
+	for(i=0;i<NB_LIGNES;i++){
+		if(grid[i][NB_CLOS-1]!=0 && grid[i][NB_COLS-1]!=1){
+			return 0;
+		}
+	}
+	for(j=NB_COLS-1;j>=0;j--){
+		for(i=0;j<NB_LIGNES;i++){
+			if(grid[i][j]!=0 && grid[i][j]!=1){
+				grid[i][j+1] = grid[i][j];
+				grid[i][j] = 0;
+			}
+		}
+	}
+}
+	
